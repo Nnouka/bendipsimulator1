6f81ecb73195f5b5b87f5307cbcd3963def34c3d
Nouks <nnouka.stephen@go-groups.net>
2019-09-15 01:12:18 +0100

    added postgres database config

--- config/database.php
+++ config/database.php
@@ -1,5 +1,5 @@
 <?php
-
+$DATABASE_URL=parse_url("postgres://bcnaplwvswltbg:f182cec31608a2749e2f53d57deeb1dbc6540c71130e4ab5d1909e00dad5611d@ec2-54-225-205-79.compute-1.amazonaws.com:5432/dc0lks9q8n407c");
 return [
 
     /*
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -61,16 +61,16 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
